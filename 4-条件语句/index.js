91d24514beabe601dacc219e082ac8ac3c266489
JcGam1ng <2323024922@qq.com>
2025-08-31 00:27:24 +0800

    25/08/30

--- 4-条件语句/index.js
+++ 4-条件语句/index.js
@@ -9,3 +9,12 @@ if(10 === l_iTen)
 {
     console.log("=== 考虑类型");
 }
+
+console.log("三目运算符 和 C++ 一样");
+
+console.log("For 循环:");
+var l_arrNums = [1,2,3];
+for(var l_iNum of l_arrNums)
+{
+    console.log(l_iNum);
+}
