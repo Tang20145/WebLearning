3a5b55089d881ac079bd7db7d672eafda2cadbc6
JcGam1ng <2323024922@qq.com>
2025-08-30 23:56:44 +0800

    25/08/30

---
+++ 4-条件语句/index.js
@@ -0,0 +1,11 @@
+console.log("=========条件语句==========");
+var l_iTen = 10;
+var l_strTen = "10";
+if(10 == l_strTen)
+{
+    console.log("== 不考虑类型");
+}
+if(10 === l_iTen)
+{
+    console.log("=== 考虑类型");
+}
